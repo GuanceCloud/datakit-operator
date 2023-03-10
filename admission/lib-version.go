52d87d3dd79131a893b401cb95b333ccb3efc7b0
liguozhuang <liguozhuang@jiagouyun.com>
2023-03-11 01:08:21 +0800

    update lib-version

--- admission/lib-version.go
+++ admission/lib-version.go
@@ -4,7 +4,7 @@ package admission
 const (
 	javaLibVerion    = "v1.8.4-guance"
 	pythonLibVersion = "v1.6.2"
-	jsLibVerion      = "v3.9.3"
+	jsLibVerion      = "v3.9.2"
 
 	imageRegistry = "pubrepo.jiagouyun.com/datakit-operator"
 )
