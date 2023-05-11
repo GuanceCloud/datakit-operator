b634a66d9c9641a5809870396b451f2422db2d80
leecha <22087646+leecha@users.noreply.github.com>
2023-05-11 18:21:59 +0800

    Dev iss15 operator election

---
+++ apis/election/election.go
@@ -0,0 +1,93 @@
+package election
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/luci/go-render/render"
+)
+
+const (
+	jsonContentType = "application/json"
+)
+
+var globalTaskManager = newTaskManager()
+
+func HandleElection(w http.ResponseWriter, r *http.Request) {
+	handle(w, r)
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	var body []byte
+	if r.Body != nil {
+		if data, err := ioutil.ReadAll(r.Body); err == nil {
+			body = data
+		}
+	}
+
+	// contentType := r.Header.Get("Content-Type")
+	// if contentType != jsonContentType {
+	// 	l.Errorf("contentType=%s, expect application/json", contentType)
+	// 	return
+	// }
+
+	l.Debugf("handling request: %s", body)
+
+	requ := &electionRequest{}
+	if err := json.Unmarshal(body, requ); err != nil {
+		msg := fmt.Sprintf("Request could not be decoded: %v", err)
+		l.Error(msg)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	result, err := processElection(requ)
+	if err != nil {
+		msg := fmt.Sprintf("Invalid election parameter: %v", err)
+		l.Error(msg)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+
+	}
+
+	respBytes, err := json.Marshal(result)
+	if err != nil {
+		l.Error(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", jsonContentType)
+	if _, err := w.Write(respBytes); err != nil {
+		l.Error(err)
+	}
+}
+
+type electionRequest struct {
+	Namespace         string         `json:"namespace,omitempty"`
+	ID                string         `json:"id"`
+	Timestamp         int64          `json:"timestamp,omitempty"`
+	ApplicationInputs map[string]int `json:"application_inputs"`
+	RunningInputs     []string       `json:"running_inputs,omitempty"`
+}
+
+type electionResult struct {
+	AllowedInputs []string `json:"allowed_inputs"`
+}
+
+func processElection(requ *electionRequest) (*electionResult, error) {
+	globalTaskManager.mu.Lock()
+	defer globalTaskManager.mu.Unlock()
+
+	if err := globalTaskManager.checkList(requ.ApplicationInputs); err != nil {
+		return nil, err
+	}
+
+	allowed := globalTaskManager.takeTask(requ.ID)
+	l.Debugf("ID %s is allowed to run %s", requ.ID, allowed)
+	l.Debugf("workers info: %#v", render.Render(globalTaskManager.workers))
+
+	return &electionResult{AllowedInputs: allowed}, nil
+}
