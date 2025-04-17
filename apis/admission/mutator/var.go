c6520664f0bdb266d976ebc521f8943b7a9c8e15
leecha <22087646+leecha@users.noreply.github.com>
2025-04-17 18:50:44 +0800

    update logging configs

--- apis/admission/mutator/var.go
+++ apis/admission/mutator/var.go
@@ -5,11 +5,7 @@ import (
 )
 
 var (
-	loggingMatchNamespaceForConfig = func(ns string) string {
-		return config.Cfg.AdmissionMutate.Loggings.MatchNamespace(ns)
-	}
-
-	loggingMatchLabelsForConfig = func(labels map[string]string) string {
-		return config.Cfg.AdmissionMutate.Loggings.MatchLabels(labels)
+	loggingMatchNamespaceOrLabelsForConfig = func(ns string, labels map[string]string) string {
+		return config.Cfg.AdmissionMutate.Loggings.Matches(ns, labels)
 	}
 )
